3b27245373d06f8e09704f61a277f7b1a6342b65
minthuhlaing <minthuhlaing786@gmail.com>
2024-09-24 00:10:57 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="showModal">
+    <Modal :theme='theme' @close="showModal=false">
+    <h1>Hello World</h1>
+    <h3>My name is John Doe</h3>
+    <p>Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Ipsum ea amet minus, ullam earum omnis in, quidem, at beatae tenetur iure veritatis ad sapiente. Adipisci asperiores labore voluptatibus, commodi similique.</p>
+    <template v-slot:ul>
+      <ul>
+        <li>One </li>
+        <li>Two </li>
+        <li>Three </li>
+        <li>Four</li>
+      </ul>
+    </template>
+  </Modal>
+  </div>
+  <div v-if="deleteModal">
+    <Modal :themeone='themeone' @delete="deleteModal=false">
+    <h1>Are you sure you want to Delete</h1>
+    <h3>Get the fuck out of my Page</h3>
+    <p>FUCK YOU</p>
+    <template v-slot:ul>
+      <ul>
+        <li>One </li>
+        <li>Two </li>
+        <li>Three </li>
+        <li>Four</li>
+      </ul>
+    </template>
+  </Modal>
+  </div>
+  <button @click="showModal=true">Login Modal</button>
+  <button @click="deleteModal=true">Delete Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      theme:"success",
+      showModal:false,
+      themeone:"delete",
+      deleteModal: false
+    }
+  },
+components:{
+  Modal
+}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{success:theme==='success'}">
+      <slot/>
+      <slot name="ul" />
+    </div>
+  </div>
+   <div class="backdrop" @click.self="deleteModal">
+    <div class="modal" :class="{danger:themeone==='delete'}">
+      <slot/>
+      <slot name="ul" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+props:["theme","themeone"],
+methods:{
+  closeModal(){
+    this.$emit("close");
+  },
+  deleteModal(){
+    this.$emit("delete");
+  }
+}
+}
+</script>
+
+<style lang="css" scoped>
+.backdrop{
+  width:100%;
+  height:100%;
+  background:rgba(0,0,0,0.5);
+  position:fixed;
+  top:0;
+}
+.modal{
+  padding:20px;
+  margin:200px auto;
+  width:800px;
+  background:white;
+  border-radius:20px;
+}
+.success{
+  background: green;
+  color: white
+}
+.danger{
+  background: darkred;
+  color: white
+}
+</style>
